a7566f8ff68a5482d85098bda627608418af8465
chris <christopher.hawkins@cohaesus.co.uk>
2016-03-02 17:56:36 +0000

    made some extra changes to determine size on viewport

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
+var y = window.outerWidth;
 $(document).ready(function(){
     // Set the interval to be 5 seconds
     var t = setInterval(function(){
-        $("#my_carousel ul").animate({marginLeft:-1300},1000,function(){
+        $("#my_carousel ul").animate({marginLeft:-y },1000,function(){
             $(this).find("li:last").after($(this).find("li:first"));
             $(this).css({marginLeft:0});
         })
     },5000);
+});
+
+$(document).ready(function(){
+ $(".picture_slide").css("width", y);
+ $(".slider").css("width", y*3);
 });
