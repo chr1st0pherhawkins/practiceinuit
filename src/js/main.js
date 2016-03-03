fdc73cdd27a4cc0b00a9d19283c115b839187e5b
chris <christopher.hawkins@cohaesus.co.uk>
2016-03-03 17:32:37 +0000

    made overall changes to the project

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-var y = window.outerWidth;
-//var y = $(window).width();
+//var y = window.outerWidth;
+var y = $(window).width();
 $(document).ready(function(){
     // Set the interval to be 5 seconds
     var t = setInterval(function(){
         $("#my_carousel ul").animate({marginLeft:-y },1000,function(){
-            $(this).find("li:last").after($(this).find("li:first"));
+            $(this).find(".picture_slide:last").after($(this).find(".picture_slide:first"));
             $(this).css({marginLeft:0});
         })
     },5000);
