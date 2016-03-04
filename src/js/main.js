e72e29a01558750eaf16e874156edba31dfbec29
chris <christopher.hawkins@cohaesus.co.uk>
2016-03-04 12:38:59 +0000

    changes for the day to make content more visually pleasing

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
-//var y = window.outerWidth;
+//var y = window.innerWidth;
 var y = $(window).width();
 $(document).ready(function(){
     // Set the interval to be 5 seconds
     var t = setInterval(function(){
-        $("#my_carousel ul").animate({marginLeft:-y },1000,function(){
-            $(this).find(".picture_slide:last").after($(this).find(".picture_slide:first"));
-            $(this).css({marginLeft:0});
-        })
+    	$("#my_carousel ul").animate({marginLeft:-y },1000,function(){
+    		$(this).find(".picture_slide:last").after($(this).find(".picture_slide:first"));
+    		$(this).css({marginLeft:0});
+    	})
     },5000);
 });
 
 $(document).ready(function(){
- $(".picture_slide").css("width", y);
- $(".slider").css("width", y*3);
+	$(".picture_slide").css("width", y);
+	$(".slider").css("width", y*3);
 });
 
 
